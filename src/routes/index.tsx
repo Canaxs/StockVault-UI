844b26ca9471e577317f8edd734c214eb0b1aeb5
Meriç Cana <mmericcana@gmail.com>
2025-10-22 22:00:42 +0300

    framer motion add routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { publicRoutes } from "./public";
 import { protectedRoutes } from "./protected";
 import { useAuth } from "../features/auth/hooks/useAuth";
 import { useRoutes } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 export const AppRoutes = () => {
   const { isAuthenticated, isAdmin } = useAuth();
@@ -13,5 +14,24 @@ export const AppRoutes = () => {
     ...commonRoutes(),
   ];
 
-  return useRoutes(routes);
+  const element = useRoutes(routes);
+
+  return (
+    <div className="overflow-x-hidden">
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0, x: -40 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 40 }}
+          transition={{
+            duration: 0.3,
+            ease: "easeInOut",
+          }}
+        >
+          {element}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
 };
